c2859388f67c81014c66d78bb42015a05d320ab9
fangtao <swordft@163.com>
2017-12-26 08:27:27 +0800

    201712260827

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask,redirect,render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 app = Flask(__name__)
-manager = Manager(app)
+#manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -13,6 +13,14 @@ def index():
 def user(name):
     return render_template('user.html',name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
 if __name__ == '__main__':
-#    app.run(host='0.0.0.0',port=9092,debug=True)
-    manager.run()
+    app.run(host='0.0.0.0',port=9092,debug=True)
+#    manager.run()
